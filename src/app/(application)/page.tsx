56186e1b0c3a4ada5dc5af14a8d7ea8ad43efac2
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2024-02-21 14:06:22 -0600

    add shadcn/ui components

---
+++ src/app/(application)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui/button"
+
+export default function Page() {
+  return (
+    <div>
+      <h2 className="font-serif text-xl font-semibold">Hello, world</h2>
+      <Button>Noop</Button>
+    </div>
+  )
+}
