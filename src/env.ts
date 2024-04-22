08681666ecb38aacac478f722f96e4662c123d9c
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2024-04-22 17:30:54 -0600

    refactor: migrate env file to @t3-oss/env-nextjs

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,43 @@
-import { object, optional, parse, picklist, string } from "valibot"
+import { vercel } from "@t3-oss/env-core/presets"
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
 
-const envSchema = object({
-  NODE_ENV: optional(
-    picklist(["development", "production", "test"]),
-    "development",
-  ),
-  DATABASE_URL: string(),
-  DATABASE_AUTH_TOKEN: optional(string()),
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
+    DATABASE_URL: z.string().url(),
+    DATABASE_AUTH_TOKEN: z.string().optional(),
+
+    UPLOADTHING_SECRET: z
+      .string()
+      .refine(
+        (str) => !str.includes("YOUR_SECRET_HERE"),
+        "You forgot to change the default secret",
+      ),
+    UPLOADTHING_APP_ID: z
+      .string()
+      .refine(
+        (str) => !str.includes("YOUR_APP_ID_HERE"),
+        "You forgot to change the default app id",
+      ),
+  },
+  client: {},
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {
+    // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  },
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  /**
+   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
+   * `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
+  // Extend the Vercel preset.
+  extends: [vercel()],
 })
-
-export const env = parse(envSchema, process.env)
